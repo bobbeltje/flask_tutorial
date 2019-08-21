f7009571b5c6dac84747d324e858fca858ecc704
Matthias <matthiaswubs@gmail.com>
2019-08-21 22:03:51 +0200

    vid02 18:35 templates

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,16 +1,32 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# dummy data, usually you would get this from a database
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 # add a second decorator to have both pointing at the same thing
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
    
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 # to run the app without setting environmental variables
 if __name__ == '__main__':
