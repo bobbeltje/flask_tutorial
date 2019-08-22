7c72d6ba219a5bb98049093faf40c05b2f2884ec
Matthias <matthiaswubs@gmail.com>
2019-08-22 21:37:15 +0200

    vid03 completed registration and login form

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,12 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+# important
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = 'beaadd39d35e24a7746cce7a6397632a'
 
 # dummy data, usually you would get this from a database
 posts = [
@@ -27,6 +33,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 # to run the app without setting environmental variables
 if __name__ == '__main__':
